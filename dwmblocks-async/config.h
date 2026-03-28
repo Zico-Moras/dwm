397ed791e7a0f34431be6ce16d1ad3790ac058f4
Zico-Moras <francis.ffmm@proton.me>
2026-03-28 18:53:42 +0000

    Convert dwmblocks-async from submodule to regular files

---
+++ dwmblocks-async/config.h
@@ -0,0 +1,28 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// String used to delimit block outputs in the status.
+#define DELIMITER " | "
+
+// Maximum number of Unicode characters that a block can output.
+#define MAX_BLOCK_OUTPUT_LENGTH 45
+
+// Control whether blocks are clickable.
+#define CLICKABLE_BLOCKS 1
+
+// Control whether a leading delimiter should be prepended to the status.
+#define LEADING_DELIMITER 0
+
+// Control whether a trailing delimiter should be appended to the status.
+#define TRAILING_DELIMITER 0
+
+// Define blocks for the status feed as X(icon, cmd, interval, signal).
+#define BLOCKS(X)             \
+    X("", "sb-redshift", 60, 11) \
+    X("", "sb-cputemp",     5,  0)  \
+    X("", "sb-batt",     30, 0)  \
+    X("", "sb-blue",     5,  12)  \
+    X("", "sb-net",      5,  0)  \
+    X("", "sb-audio",    0,  10) \
+    X("", "date '+%a, %b %d %H:%M'", 60, 0)
+#endif  // CONFIG_H
