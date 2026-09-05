dbf63af397182c73e617fac58b8ba6d520da2da3
Zico-Moras <francis.ffmm@proton.me>
2026-09-05 15:12:02 +0200

    wallpaper things

--- startdwm.sh
+++ startdwm.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 # Set the wallpaper in the background
-feh --bg-scale ~/dwm/wallpaper_2.png
+feh --bg-scale ~/dwm/wallpapers/day/wallpaper_fallen_angel_close.jpg
+
+# Rotate/check wallpaper every 5 minutes
+#(
+#  while true; do
+#    ~/scripts/set_wallpaper.sh
+#    sleep 300
+#  done
+#) &
 
 while true; do
 	# Log stderror to a file
